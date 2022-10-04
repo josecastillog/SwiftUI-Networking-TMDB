bf3a4600b62716fcf0ceec0a89f59fd86804e7db
josecas <jmcg0901@gmail.com>
2022-10-04 11:49:58 -0500

    Initial Commit

---
+++ NetworkPlayground/NetworkPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkPlaygroundApp.swift
+//  NetworkPlayground
+//
+//  Created by Jose Castillo on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct NetworkPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
